747bbde68e730045d89d5b25e1c0373675ce2163
Lahiru Udawaththa <45584797+lahiruroot@users.noreply.github.com>
2020-10-01 06:30:15 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -8,10 +8,10 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home"),),
+      appBar: AppBar(title: Text("PAGE"),),
       body: Center(
        child: Container(
-        child: Text("This is home"),
+        child: Text("The First page"),
       ),
       )
       
